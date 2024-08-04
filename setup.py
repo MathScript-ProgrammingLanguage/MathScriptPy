400c041a853468c02d388b787b46592a11c78b3c
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-08-04 18:32:31 +0200

    Prevent MyPy from complaining

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from cx_Freeze import setup, Executable
+from cx_Freeze import setup, Executable # type: ignore
 import mathscript
 import subprocess
 
@@ -20,7 +20,7 @@ with open('shell.py', 'r+') as f:
     try:
         git_commit_date = subprocess.check_output(['git', 'show', '-s', '--format=%cd', '--date=format:"%b %e %Y, %H:%M:%S"', 'HEAD']).decode('utf-8').strip().removeprefix('"').removesuffix('"')
     except subprocess.CalledProcessError as e:
-        git_commit_date = None
+        git_commit_date = None # type: ignore
 
     content = content.replace("git_release_tag = 'latest'\ngit_commit_hash = 'latest'\ngit_commit_date = None", f'git_release_tag = {repr(git_release_tag)}\ngit_commit_hash = {repr(git_commit_hash)}\ngit_commit_date = {repr(git_commit_date)}')
 
@@ -28,7 +28,7 @@ with open('shell.py', 'r+') as f:
     f.write(content)
     f.truncate()
 
-build_options = {'packages': [], 'excludes': []}
+build_options = {'packages': [], 'excludes': []} # type: ignore
 
 base = 'console'
 
